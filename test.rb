3248ee7998a818c3f9c5f1725a399c4d0f8aa031
whfrog <whfrog@naver.com>
2016-04-20 17:36:35 +0900

    update test file in testing branch

---
+++ test.rb
@@ -0,0 +1,8 @@
+i make testing branch
+i fill with some contents
+i changed this file on master branch
+i check this change on testing branch
+
+i change this contents on testing branch
+i check this change on master branch
+i expect thntn master branch there is no change 
